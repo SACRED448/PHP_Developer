30f1afeba00e7f7d5b9e06356cf18d852646b0d5
Sergey <pavlovsergey64@gmail.com>
2023-02-11 22:57:44 +0700

    Добавлена кнопка для удаления записей в реалтайме

--- Plugin_PHP/history.php
+++ Plugin_PHP/history.php
@@ -5,11 +5,20 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/styles.css">
     <title>История введённых данных</title>
 </head>
 <body>
-
 <h1>История введённых данных:</h1>
 
+<script>
+    function clearPage () { //Очиска истории, но только в реалтайме
+        const div = document.querySelector('div');
+        const parent = div.parentNode;
+        parent.removeChild(div);
+    }
+</script>
+
+<input value = "Очистить историю" class="button_remove" onclick="clearPage()" type="button">
+
 </body>
-<div>a = 1 b = 1 c = 1 D = -3</div><br><br><div>a = 1 b = 1 c = 1 D = -3</div><br><br><div>a = 1 b = 1 c = 1 D = -3</div><br><br><div>a = 1 b = 1 c = 1 D = -3</div><br><br><div>a = 1 b = 1 c = 1 D = -3</div><br><br><div>a = 1 b = 1 c = 1 D = -3</div><br><br><div>a = 1 b = 1 c = 1 D = -3</div><br><br><div>a = 1 b = 1 c = 1 D = -3</div><br><br><div>a = 1 b = 1 c = 1 D = -3</div><br><br><div>a = 1 b = 1 c = 1 D = -3</div><br><br><div>a = 1 b = 1 c = 1 D = -3</div><br><br><div>a = 1 b = 1 c = 1 D = -3</div><br><br><div>a = 1 b = 1 c = 1 D = -3</div><br><br><div>a = 1 b = 1 c = 1 D = -3</div><br><br>
